773c5dcf769b264b408b75429b435a136dfa751c
Dylan Weijgertze <41340521+dylanwe@users.noreply.github.com>
2023-03-14 09:12:03 +0100

    finished day 2 part 2

--- src/main/kotlin/days/day02/Day02.kt
+++ src/main/kotlin/days/day02/Day02.kt
@@ -20,6 +20,36 @@ fun part1(input: List<String>): Int {
     return points
 }
 
+fun part2(input: List<String>): Int {
+	var points = 0
+
+	for (outcome in input) {
+		val (otherMove, _, yourMove) = outcome.toCharArray()
+		val outcome = when (yourMove) {
+			'Y' -> 3 // draw
+			'Z' -> 6 // win
+			else -> 0 // loss
+		}
+
+        // possible scores
+        val scores = arrayOf(1,2,3)
+
+        // base move
+        val base = 'A'
+
+        // calculate move value based on win or loss
+        val move = when(outcome) {
+            3 -> scores[otherMove - base]
+            6 -> scores[((otherMove - base) + 1) % scores.size]
+            else -> scores[((otherMove - base) + 2) % scores.size]
+        }
+
+        points += move + outcome
+	}
+
+	return points
+}
+
 fun main() {
     val input = File("src/main/kotlin/days/day02/input.txt").readLines()
     println(part1(input))
